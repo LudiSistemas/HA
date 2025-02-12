7ba450aeb1593dd06bf93af236d75dab6e61696d
Ludi Sistemas <portalscg@gmail.com>
2025-02-12 16:46:06 +0100

    udnoing android app

--- android/WeatherStation/app/src/main/java/com/weatherstation/data/model/HistoricalData.kt
+++
@@ -1,9 +0,0 @@
-data class HistoricalData(
-    val min: Double?,
-    val max: Double?,
-    val current: Double?,
-    val history: List<SensorData>,
-    val startTime: String,
-    val endTime: String,
-    val hasMore: Boolean
-) 
