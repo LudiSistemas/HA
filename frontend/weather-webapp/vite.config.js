6d5957d0d45072159e506fbee3d7094193aada27
Ludi Sistemas <portalscg@gmail.com>
2025-02-13 19:08:20 +0100

    adding back .env the proper way

--- frontend/weather-webapp/vite.config.js
+++ frontend/weather-webapp/vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true, // Enable sourcemaps for debugging
-    minify: 'terser',
-  },
-  server: {
-    port: 3000,
-  },
-  // Make env variables available
-  define: {
-    __BACKEND_URL__: JSON.stringify(env.VITE_BACKEND_URL),
-    'process.env': {}
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true, // Enable sourcemaps for debugging
+      minify: 'terser',
+    },
+    server: {
+      port: 3000,
+    },
+    // Make env variables available
+    define: {
+      __BACKEND_URL__: JSON.stringify(env.VITE_BACKEND_URL),
+      'process.env': {}
+    }
   }
 }) 
